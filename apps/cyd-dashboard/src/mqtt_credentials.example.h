0ccf7168cbaa5352fe790db08246bb5fefb1b903
Tony Perkins <perkins.tony@gmail.com>
2026-07-29 22:31:50 -0500

    chore(cyd): support local MQTT credential provisioning

---
+++ apps/cyd-dashboard/src/mqtt_credentials.example.h
@@ -0,0 +1,4 @@
+// Copy this file to mqtt_credentials.h only on the computer flashing a CYD.
+// mqtt_credentials.h is ignored by Git and must never be committed.
+#define MQTT_USERNAME ""
+#define MQTT_PASSWORD ""
